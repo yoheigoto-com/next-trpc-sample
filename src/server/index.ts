cb09abeb79f224b0cfb33923843c5f4a97016a53
yoheigoto-com <dev@yoheigoto.com>
2025-03-29 06:39:02 +0900

    TRPCを使用したAPIルートとアプリケーションルーターを追加

---
+++ src/server/index.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+import { publicProcedure, router } from "./trpc";
+
+export const appRouter = router({
+  greeting1: publicProcedure.query((opts) => {
+    return { message: "Hello World!" };
+  }),
+  greeting2: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .query((opts) => {
+      return { message: `Hello ${opts.input.name ?? "World!"} ` };
+    }),
+});
+
+export type appRouter = typeof appRouter;
